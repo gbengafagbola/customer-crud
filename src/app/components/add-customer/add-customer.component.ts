f7ed21d2443dfe825e1816098ecf0015d0dadf13
oluwagbenga fagbola <fagbolagbenga2015@gmail.com>
2021-04-17 20:28:44 +0100

    third commit

--- src/app/components/add-customer/add-customer.component.ts
+++ src/app/components/add-customer/add-customer.component.ts
@@ -1,7 +1,5 @@
 import { Router } from '@angular/router';
 import { Component, OnInit, ViewChild, NgZone } from '@angular/core';
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { MatChipInputEvent } from '@angular/material/chips';
 import { ApiService } from './../../shared/api.service';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
@@ -21,12 +19,8 @@ export class AddCustomerComponent implements OnInit {
   selectable = true;
   removable = true;
   addOnBlur = true;
-  @ViewChild('chipList', { static: true }) chipList;
   @ViewChild('resetCustomerForm', { static: true }) myNgForm;
-  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
   customerForm: FormGroup;
-  subjectArray: Subject[] = [];
-  SectioinArray: any = ['A', 'B', 'C', 'D', 'E'];
 
   ngOnInit() {
     this.submitBookForm();
@@ -44,33 +38,15 @@ export class AddCustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       student_name: ['', [Validators.required]],
       customer_address: ['', [Validators.required]],
-      subjects: [this.subjectArray],
+      order_number: ['', [Validators.required]],
+      postal_code: ['', [Validators.required]],
+      house_number: ['', [Validators.required]],
+      order_amount: ['', [Validators.required]],
       dob: ['', [Validators.required]],
       gender: ['Male']
     })
   }
 
-  /* Add dynamic languages */
-  add(event: MatChipInputEvent): void {
-    const input = event.input;
-    const value = event.value;
-    // Add language
-    if ((value || '').trim() && this.subjectArray.length < 5) {
-      this.subjectArray.push({ name: value.trim() })
-    }
-    // Reset the input value
-    if (input) {
-      input.value = '';
-    }
-  }
-
-  /* Remove dynamic languages */
-  remove(subject: Subject): void {
-    const index = this.subjectArray.indexOf(subject);
-    if (index >= 0) {
-      this.subjectArray.splice(index, 1);
-    }
-  }  
 
   /* Date */
   formatDate(e) {
--- src/app/shared/customer.model.ts
+++ src/app/shared/customer.model.ts
@@ -1,9 +1,11 @@
 export class Customer {
   _id: String;
-  student_name: String;
-  student_email: String;
-  section: String;
-  subjects: Array<string>;
+  customer_name: String;
+  customer_address: String;
+  house_number: String;
+  postal_code: String;
+  order_number: String;
+  order_amount: String;
   dob: Date;
   gender: String;
 }
